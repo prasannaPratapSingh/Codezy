6b1385e3a176eccb37ded2d2b4cb927a6f9f02bb
Prasanna Pratap Singh <prap.sin.25@gmail.com>
2025-10-19 00:28:54 +0530

    Refactor HeroSection component for improved animations

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -98,7 +98,7 @@ function HeroSection() {
         </div>
         <div className="mt-5 flex flex-col items-center leading-tight">
           <div ref={titleanim} >
-            <span className=" text-5xl sm:text-8xl">
+            <span className=" text-5xl sm:text-8xl font-white">
               Craft Your
             </span>
           </div>
@@ -124,3 +124,4 @@ function HeroSection() {
 }
 
 export default HeroSection
+
