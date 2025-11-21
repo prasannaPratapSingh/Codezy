312db560d6a186a0861b98d0dcfc5fe41b6dd10f
Prasanna Pratap Singh <prap.sin.25gmail.com>
2025-11-22 00:27:07 +0530

    models-for-contest-feature

---
+++ backend/src/models/contestSchema.js
@@ -0,0 +1,117 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const contestSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        requried: true
+    },
+    startTime: {
+        type: Date,
+        required: true
+    },
+    endTime: {
+        type: Date,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['draft', 'active', 'completed', 'cancelled'],
+        default: 'draft'
+    },
+
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        enum: ['easy', 'medium', 'hard'],
+        required: true
+    },
+    tags: {
+        type: String,
+        enum: ['array', 'linkedList', 'graph', 'dp'],
+        required: true
+    },
+    points: {
+        stype: Number,
+        required: true,
+        min: 0
+    },
+    visibleTestCases: [
+        {
+            input: {
+                type: String,
+                required: true,
+            },
+            output: {
+                type: String,
+                required: true,
+            },
+            explanation: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+
+    hiddenTestCases: [
+        {
+            input: {
+                type: String,
+                required: true,
+            },
+            output: {
+                type: String,
+                required: true,
+            }
+        }
+    ],
+
+    startCode: [
+        {
+            language: {
+                type: String,
+                required: true,
+            },
+            initialCode: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+
+    referenceSolution: [
+        {
+            language: {
+                type: String,
+                required: true,
+            },
+            completeCode: {
+                type: String,
+                required: true
+            }
+        }
+    ],
+    contestCreator: {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
+    },
+    generatedBy: {
+        type: String,
+        default: 'admin'
+    }
+
+}, {
+    timestamps: true
+})
+
+
+const Contest = mongoose.model('contest', contestSchema);
+module.exports = Contest;
---
+++ backend/src/models/contestSubmissionSchema.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const contestSubmissionSchema=new Schema({
+    contestId:{
+        type:Schema.Types.ObjectId,
+        ref:'contest',
+        required:true
+    },
+    userId:{
+        type:Schema.Types.ObjectId,
+        ref:'user',
+        required:true
+    },
+    //ab yaha se contest ki information hogi
+    language:{
+        type:String,
+        required:true
+    },
+    sourceCode:{
+        type:String,
+        required:true
+    },
+    testCaseResults:[{
+        testCaseType:{
+            type:String,
+            enum:['visible','hidden'],
+            required:true
+        },
+        input:String,
+        expectedOutput:String,
+        actualOutput:String,
+        passed:{
+            type:Boolean,
+            required:true
+        },
+        executionTime:Number,
+        memoryUsed:Number,
+        errorMessage:String
+    }],
+    totalTestCases:{
+        type:Number,
+        default:0
+    },
+    passedTestCases:{
+        type:Number,
+        default:0
+    },
+    score:{
+        type:Number,
+        default:0
+    },
+
+    isAccepted:{
+        type:Boolean,
+        default:false
+    }
+},{
+    timestamps:true
+})
