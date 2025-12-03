381a1dd94a4953a8861b6176124ee48f3d6a44ac
Prasanna Pratap Singh <prap.sin.25gmail.com>
2025-12-04 02:36:20 +0530

    bug-fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,14 +19,14 @@ const contestRouter=require('./routes/contest');
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://codezy-6y04.onrender.com',
     credentials: true
 }))
 
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:5173',
+        origin: 'https://codezy-6y04.onrender.com',
         methods: ['GET', 'POST'],
         credentials: true
     }
