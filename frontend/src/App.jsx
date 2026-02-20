261524c6846e04bdde2bf921a145591cccc6a44f
Prasanna <prap.sin.25@gmail.com>
2026-02-20 10:17:58 +0530

    final-bugfix-loading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -59,7 +59,7 @@ function App() {
 
 
 
-  if (!loading) {
+  if (loading) {
     return <div className="min-h-screen flex items-center justify-center">
       <span className="loading loading-spinner loading-lg"></span>
     </div>;
