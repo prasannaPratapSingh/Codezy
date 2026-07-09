a8db7dbb7149f94cb0a8806aa34e4bf6c8454d4a
Prasanna Pratap Singh <prap.sin.25@gmail.com>
2026-07-09 23:53:09 +0530

    Update axios base URL to include 'www'

--- frontend/src/utils/axiosClient.js
+++ frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosClient = axios.create({
-    baseURL: 'https://codezy.space',
+    baseURL: 'https://www.codezy.space',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
